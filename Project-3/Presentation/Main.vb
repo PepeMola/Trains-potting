c3533702d9458e51d598c5a39cb41c8d36497bf1
PAULINO DE LA FUENTE LIZCANO <Paulino.Lafuente@alu.uclm.es>
2020-04-16 12:38:19 +0200

    prices solve, solo queda trips y querries

--- Project-3/Presentation/Main.vb
+++ Project-3/Presentation/Main.vb
@@ -332,10 +332,19 @@
         Dim pro As Product
 
         If Me.txtEurosPrices.Text <> Nothing Then
-            pro = New Product(cboxProductPrices.SelectedItem.ToString)
-            pro.ReadProductDescription()
-            pri = New Prices(pro.ProductID, dtpDatePrices.Text)
-            pri.EurosPerTon = Convert.ToDouble(Replace(txtEurosPrices.Text, ",", "."))
+            'Checking if the data introduce is right or not
+            Try
+                pro = New Product(cboxProductPrices.SelectedItem.ToString)
+                pro.ReadProductDescription()
+                pri.ProductID = pro.ProductID
+                pri.PriceDate = dtpDatePrices.Text
+                pri.EurosPerTon = Convert.ToDouble(Replace(txtEurosPrices.Text, ".", ","))
+            Catch ex As Exception
+                MessageBox.Show("Be careful with the data you have introduce", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                txtEurosPrices.Text = String.Empty
+                dtpDatePrices.Text = String.Empty
+                Exit Sub
+            End Try
 
             Try
                 If pri.UpdatePrice() <> 1 Then
