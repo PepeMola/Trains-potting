70abbd97bfca26d5104b2f94a06b0749ca1c2305
Pepe Arias Ramos <pepe.rocks9@gmail.com>
2020-04-24 21:01:12 +0200

    Ya estaria la query4

--- Project-3/Persistence/Query4DAO.vb
+++ Project-3/Persistence/Query4DAO.vb
@@ -13,15 +13,22 @@
     Public Sub ReadQuery4(ByRef q As Query4)
         Dim col As Collection : Dim aux As Collection
         Dim productsCol As Collection
-        col = DBBroker.GetBroker.Read("SELECT t1.TripDate, t1.Train, tr1.TrainTypeDescription, SUM(t1.TonsTransported*pr1.EurosPerTon) AS MaxProfit
-                                        FROM Trips AS t1, Products AS p1, Prices AS pr1, TrainTypes tr1, Trains tra
-                                        WHERE t1.Product = p1.ProductID AND p1.ProductID = pr1.Product AND t1.Train =tra.TrainID AND tra.TrainType = tr1.TrainTypeID
-                                        GROUP BY t1.TripDate, t1.Train, tr1.TrainTypeDescription
-                                        HAVING SUM(t1.TonsTransported*pr1.EurosPerTon) =
-                                            (SELECT MAX(tr.Profit) 
-                                             FROM (SELECT t.TripDate, t.Train, SUM(t.TonsTransported*pr.EurosPerTon) AS Profit FROM Trips AS t, Products AS p, Prices AS pr 
-                                             WHERE t.Product = p.ProductID AND p.ProductID = pr.Product  AND pr.PriceDate < t.TripDate
-                                             GROUP BY t.TripDate, t.Train)  AS tr);")
+        col = DBBroker.GetBroker.Read("SELECT  t2.TripDate, t2.Train, ty.TrainTypeDescription, SUM(t2.TonsTransported*pr2.EurosPerTon) As MaxProfit 
+                                        FROM (SELECT MAX(pr3.PriceDate) As LatestDate, p3.ProductID As Product
+                                              FROM Prices pr3, Products p3
+                                              WHERE p3.ProductID = pr3.Product
+                                              GROUP BY  p3.ProductID) As q,
+                                        Prices pr2, Products p2, Trips t2, TrainTypes ty, Trains tra
+                                        WHERE pr2.PriceDate = q.LatestDate AND p2.ProductID = q.Product AND p2.ProductID = t2.Product AND pr2.Product = p2.ProductID AND tra.TrainID = t2.Train AND tra.TrainType = ty.TrainTypeID
+                                        GROUP BY t2.TripDate, t2.Train, ty.TrainTypeDescription
+                                        HAVING SUM(t2.TonsTransported*pr2.EurosPerTon) = (SELECT MAX(tr.Profit) 
+                                                                                          FROM (SELECT t.TripDate, t.Train, SUM(t.TonsTransported*pr.EurosPerTon) AS Profit FROM Trips AS t, Products AS p, Prices AS pr,
+                                                                                               (SELECT MAX(pr.PriceDate) As LatestDate, p.ProductID As Product
+                                                                                                FROM Prices pr, Products p
+                                                                                                WHERE p.ProductID = pr.Product
+                                                                                                GROUP BY  p.ProductID) As q
+                                                                                                WHERE pr.PriceDate = q.LatestDate AND p.ProductID = q.Product AND p.ProductID = t.Product AND pr.Product = p.ProductID
+                                                                                                GROUP BY t.TripDate, t.Train)  AS tr);")
 
         For Each aux In col
             Me.query.Add(FormatDateTime(aux(1), DateFormat.ShortDate).ToString)
@@ -34,10 +41,14 @@
         trip = New Trip(query(1).ToString, tr.TrainID)
         trip.ReadTrip()
 
-        productsCol = DBBroker.GetBroker.Read("SELECT p.ProductDescription, t.TonsTransported, pri.EurosPerTon, pri.PriceDate, SUM(t.TonsTransported * pri.EurosPerTon) AS Profit
-                                               FROM Products p, Trips t, Prices pri
-                                               WHERE p.ProductID = t.Product AND pri.Product = p.ProductID AND t.Train ='" & trip.Train & "' AND t.TripDate=#" & trip.TripDate & "#
-                                               GROUP BY p.ProductDescription, t.TonsTransported, pri.EurosPerTon, pri.PriceDate")
+        productsCol = DBBroker.GetBroker.Read("SELECT p2.ProductDescription, t2.TonsTransported, pr2.EurosPerTon, q.LatestDate, SUM(t2.TonsTransported*pr2.EurosPerTon) As Profit 
+                                                FROM (SELECT MAX(pr3.PriceDate) As LatestDate, p3.ProductID As Product
+                                                      FROM Prices pr3, Products p3
+                                                      WHERE p3.ProductID = pr3.Product
+                                                      GROUP BY  p3.ProductID) As q,
+                                                Prices pr2, Products p2, Trips t2
+                                                WHERE pr2.PriceDate = q.LatestDate AND p2.ProductID = q.Product AND p2.ProductID = t2.Product AND pr2.Product = p2.ProductID AND t2.TripDate = #24/04/2020#
+                                                GROUP BY p2.ProductDescription, t2.TonsTransported, pr2.EurosPerTon, q.LatestDate;")
 
         'Here we create the table format in which we store the info of the query
         solution.Columns.Add("Product Description", GetType(String))
